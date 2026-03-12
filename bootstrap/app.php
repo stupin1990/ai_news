b64e52e2e8a85a15edd41f0e6cbd9a813b7c0167
stupin1990 <stupin1990@gmail.com>
2026-03-12 22:54:07 +0700

    trustProxies update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo('/login');
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
